1871e16403da2991ee8556a59bc0b8d0b932c61f
Dona <donajeny@gmail.com>
2026-02-12 11:35:15 +0530

    Removed .history folder and added .gitignore

--- .history/emotion_detection_test_20260114115309.py
+++
@@ -1,8 +0,0 @@
-from transformers import pipeline
-
-emotion_classifier = pipeline(
-    "text-classification",
-    model="j-hartmann/emotion-english-distilroberta-base",
-    return_all_scores=True,
-    device=-1   # force CPU
-)
